533b337e12e9116ab52e584faa95e1f6949c4dc3
Michael Toyi <michael.toyi@norrenberger.local>
2024-08-12 14:22:36 +0100

    Created user routes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const {} = require('../controllers/user')
+const {registerUser, login} = require('../controllers/user')
 
 const router = express.Router();
 
-router.get('/fetch/date', getArticlesByPublishingDate);
-router.post('/create', createArticle);
+router.post('/register', registerUser);
+router.post('/login', login);
 
 
 module.exports = router;
